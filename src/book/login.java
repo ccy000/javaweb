4bc383ed0467a51dbb14be4fab48f40351c9eb0e
ccy000 <742655967@qq.com>
2019-05-26 13:11:31 +0800

    5.28

--- src/book/login.java
+++ src/book/login.java
@@ -1,16 +1,13 @@
 package book;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 @WebServlet("/login")
 public class login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+	private static final long serialVersionUID = 1L;     
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,24 +19,18 @@ public class login extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setCharacterEncoding("utf-8");
-	    response.setContentType("text/html;charset:utf-8");
-		
+	    response.setContentType("text/html;charset:utf-8");		
 		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		
-		if("admin".equals(username) && "123".equals(password)) {
-			
+		String password = request.getParameter("password");		
+		if("admin".equals(username) && "123".equals(password)) {			
 		request.getRequestDispatcher("searchbook.html").forward(request,response);
 		}
 		else
 			request.getRequestDispatcher("login.html").forward(request,response);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		doGet(request, response);
 	}
-
 }
 
 
